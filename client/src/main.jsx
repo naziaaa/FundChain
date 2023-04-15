3e6b2f48413820f70bf11b7025b117b66cf72418
name <tnazia377@gmail.com>
2023-04-15 23:35:01 +0530

    Sidebar & Navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 import App from './App';
+import './index.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
+  <ThirdwebProvider desiredChainId={ChainId.Sepolia}> 
     <Router>
         <App />
     </Router>
