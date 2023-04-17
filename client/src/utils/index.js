14196148ccafa564891dac1a765ecface7f10d9a
name <tnazia377@gmail.com>
2023-04-17 14:41:16 +0530

    Functionality working

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,20 +1,22 @@
-export const daysleft=(deadline)=>{
-    const differnce=new Date(deadline).getTime-Date.now();
-    const remainingDays = differnce/(1000 * 3600 * 24);
+export const daysLeft = (deadline) => {
+    const difference = new Date(deadline).getTime() - Date.now();
+    const remainingDays = difference / (1000 * 3600 * 24);
+  
     return remainingDays.toFixed(0);
-};
-
-export const calculateBarPercentage=(goal, raisedAmount) =>{
-    const percentage= Math.round((raisedAmount*100)/goal);
-
+  };
+  
+  export const calculateBarPercentage = (goal, raisedAmount) => {
+    const percentage = Math.round((raisedAmount * 100) / goal);
+  
     return percentage;
-};
-
-export const checkIfImage=(url, callback) =>{
-    const img= new Image;
-    img.src=url;
-
-    if(img.complete) callback(true);
+  };
+  
+  export const checkIfImage = (url, callback) => {
+    const img = new Image();
+    img.src = url;
+  
+    if (img.complete) callback(true);
+  
     img.onload = () => callback(true);
     img.onerror = () => callback(false);
-};
+  };
